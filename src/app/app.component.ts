1ce23bc06849e47de62583268569061887fafda5
sohail7866 <ah532703@gmail.com>
2020-10-20 11:26:14 +0530

    generic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'practiceMaterial';
+export class AppComponent implements OnInit {
+ 
+  constructor(){}
+
+  ngOnInit(){
+
+      let message;
+       message = 'Programming';
+
+      let endsWithG = (message as string).endsWith('g');
+      let startsWithP = (<string>message).startsWith('P');
+      let upperCase = (message as string).toUpperCase();
+      let lowerCase = (<string>message).toLocaleLowerCase(); // difference b/w ttoLocaleLowerCase() and  toLowerCase () ??
+      // let endsWithG = (<string>message).endsWith('g');
+
+      console.log("endsWithG",endsWithG);
+      console.log("startsWithP",startsWithP);
+      console.log("upperCase",upperCase);
+      console.log("lowerCase",lowerCase);
+      
+    document.addEventListener('click', evt => {
+        console.log(evt); // MultiValue Data Stream //Synchronus Opeartion
+    })
+
+    let counter = 0;
+
+    setInterval(() => {
+        counter++;
+        console.log("counter",counter);  // MultiValue Data Stream //Synchronus Opeartion
+    },1000);
+
+    setTimeout(() => {
+      console.log("completes...");  // SingleValue Data Stream //Synchronus Opeartion
+    },3000);
+
+  }
+
 }

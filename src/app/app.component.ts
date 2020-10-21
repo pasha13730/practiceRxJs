fa1517bdc03443da3c15225f0f3f9f42f0b10dfc
sohail7866 <ah532703@gmail.com>
2020-10-21 14:09:15 +0530

    created a HTTP Observable

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
+import { noop } from '@angular/compiler/src/render3/view/util';
 import { Component, OnInit } from '@angular/core';
-
-import { fromEvent, interval, timer } from 'rxjs';
+import { environment } from '../environments/environment'
+import { fromEvent, interval, observable, Observable, Observer, timer } from 'rxjs';
 
 
 @Component({
@@ -13,7 +14,32 @@ export class AppComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-      //custom Observables // HTTP Observable
-  }
+    //custom Observables // HTTP Observable
 
-}
+    //  const http$ = Observable.create((observer) => {
+     const http$ = new Observable((observer) => {
+
+       fetch(environment.SERVICE_BASE_URL)
+         .then((response) => {
+           return response.json();
+         })
+         .then((body) => {
+           observer.next(body);
+           observer.complete();
+         })
+         .catch(err => {
+           observer.error(err);
+          //  console.log(err);
+          })
+     });
+
+     http$.subscribe(
+
+       (courses) => console.log('courses => ', courses), //first callback
+        error => console.log('error =>',error),   //error handling callback
+       () => console.log('completed')  //completion callback
+     );
+
+  } //ngOnInit()
+
+}//class AppComponent

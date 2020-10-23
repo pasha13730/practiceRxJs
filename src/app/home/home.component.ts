afc677c50e7bd092407f312db98e41aa41e0863a
sohail7866 <ah532703@gmail.com>
2020-10-23 13:02:26 +0530

    reactive design - in progress

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -15,7 +15,7 @@ export class HomeComponent implements OnInit {
 
   beginnerCourses$: Observable<Course[]>;
   advanceCourses$: Observable<Course[]>;
-  beginnerCourses: Course[] = [];
+  
 
   constructor() { }
 
@@ -23,40 +23,24 @@ export class HomeComponent implements OnInit {
 
     //custom Observables // HTTP Observable
 
-    const http$ = createHttpObservable(environment.SERVICE_BASE_URL); //  const http$ = Observable.create((observer) => {  //.create is deprecated
+    const http$  = createHttpObservable(environment.SERVICE_BASE_URL); //  const http$ = Observable.create((observer) => {  //.create is deprecated
 
-    const courses$ = http$
+    const courses$ : Observable<Course[]> = http$
       .pipe(
         map(res => Object.values(res["payload"]))  //same with   map(res => res["payload"])
       );
 
     this.beginnerCourses$ = courses$
       .pipe(
-        map((courses : Course[]) => courses.filter(course => course.category == 'BEGINNER' ))
+        map((courses ) => courses.filter(course => course.category == 'BEGINNER' ))
       );
-      
-     this.beginnerCourses$.subscribe( beginnerCourse => {
-       this.beginnerCourses = beginnerCourse;
-       console.log(this.beginnerCourses);
-       console.log(beginnerCourse);
-       
-      });
 
+      this.advanceCourses$ = courses$
+      .pipe(
+        map((courses ) => courses.filter(course => course.category == 'ADVANCED' ))
+      );
      
-     
-
-
-    courses$.subscribe(
-
-      (courses) => console.log(courses), //first callback
-
-      error => console.log('error =>', error),   //error handling callback
-
-      () => console.log('completed')  //completion callback
-    );
-
-    // if we want to yse RxJs as it was meant to be , we shouldavoid writing lots of logic 
-    //inside subscribe , called imperative approach.
+      //this is a reactive approach but when inspecting 
 
 
   }

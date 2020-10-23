c12726971f57765219747c8e6b4fadc98c1d8140
sohail7866 <ah532703@gmail.com>
2020-10-23 18:47:41 +0530

    more example on concat operator

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { concat, Observable, of } from 'rxjs';
+import { concat, interval, Observable, of } from 'rxjs';
 
 @Component({
   selector: 'app-about',
@@ -12,13 +12,20 @@ export class AboutComponent implements OnInit {
 
   ngOnInit() {
 
-    const c1$ = of (1,2,3,8);
+    const c1$ = of (1,2,3,8); 
     const c2$ = of ('a','b','c','d','z');
     const c3$ = of (4,5,6,7,9);
-    
-    const c4$ = concat(c1$,c2$,c3$);
+    const c4$ = interval(1000);
 
-     c4$.subscribe(console.log); //alternative to c3$.subscribe(val => console.log(val)); 
+    // if a data stream never ends emitting value, then we will not get others data's stream output 
+    // for eg if i add a data stream that never ends , i.e., interval() see what happens
+
+    const c5$ = concat(c1$,c2$,c3$); 
+
+    //orders matter if i put c4$ first then other wont come into picture
+    // also u can write single obeservable more than once e.g. concat(c1$,c1$,c1$); 
+
+     c5$.subscribe(console.log); //alternative to c3$.subscribe(val => console.log(val)); 
 
 
   }

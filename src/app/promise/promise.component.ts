73c0b484260be0eb0caec7348ccb2fce3cf876d6
sohail7866 <ah532703@gmail.com>
2020-10-27 21:33:18 +0530

    learned document.getElementById

--- src/app/promise/promise.component.ts
+++ src/app/promise/promise.component.ts
@@ -13,22 +13,22 @@ export class PromiseComponent implements OnInit {
 
   constructor() { }
 
-  dellAvail:object = {
-      name: 'Dell',
-      color: 'Silver',
-      screenSize: '16 inch',
-      availability: true
-    }
+  dellAvail: object = {
+    name: 'Dell',
+    color: 'Silver',
+    screenSize: '16 inch',
+    availability: true
+  }
 
   hpAvail = {
-      name: 'HP',
-      color: 'Grey',
-      screenSize: '18 inch',
-      availability: true
-    }
+    name: 'HP',
+    color: 'Grey',
+    screenSize: '18 inch',
+    availability: true
+  }
 
   notAvail = {
-    status : 'Not Available in the store , Check Later',
+    status: 'Not Available in the store , Check Later',
     availability: false
   }
 
@@ -81,6 +81,61 @@ export class PromiseComponent implements OnInit {
         this.promiseVal = JSON.stringify(res);
         console.log('catch Block :', res);
       })
+
+
+
+
+    // await and async
+
+    // when we use async keyword prior to a function keyword then it always return a promise
+
+    // async function getData() {
+    //   return 'Data Recieved';
+    // }
+
+    // console.log(getData()); 
+
+    // getData().then(console.log);
+
+
+
+
+    //now lets create a new promise
+
+    let promise = new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve('Data Recieved after 2 seconds');
+      }, 2000);
+
+      setTimeout(() => {
+        box1.innerText = 'Data Fetched Successfully !!!';
+        box1.className = 'success-text';
+      }, 6000)
+    })
+
+    async function getData() {
+
+      promise.then(console.log); // one way , 2nd way below with await keyword
+
+      let response = await promise; //no need to write promise.then() block a single await keyword will do , 'await' expressions are only allowed within async functions 
+
+      // console.log('Response => ', response);
+
+      console.log(await promise);
+
+      return 'hello'
+    }
+
+    // console.log(getData()); // it will return a promise
+
+    getData();
+
+    // getData().then(console.log); // it will return whatever the getData() func is returning 'hello'
+
+
+    //learning getElementbyId thing
+
+    let box1 = document.getElementById('box1');
 
   }
 }
